38572129e55df4055f3c80279a32d22c31f20b42
Sergey Radionov <RSATom@gmail.com>
2012-10-21 11:06:57 +0700

    fixed compilation under Visual Studio

--- activex/main.cpp
+++ activex/main.cpp
@@ -52,12 +52,20 @@ using namespace std;
 
 #define COMPANY_STR "VideoLAN"
 #define PROGRAM_STR "VLCPlugin"
-#define DESCRIPTION "VLC ActiveX Plugin and IE Web Plugin"
+
+#define DESCRIPTION TEXT("VLC ActiveX Plugin and IE Web Plugin")
 
 #define THREADING_MODEL "Apartment"
 #define MISC_STATUS     "131473"
 
-#define PROGID_STR COMPANY_STR"."PROGRAM_STR
+#define PROGID_STR_A COMPANY_STR"."PROGRAM_STR
+#define PROGID_STR_W TEXT(COMPANY_STR)TEXT(".")TEXT(PROGRAM_STR)
+
+#ifdef UNICODE
+#  define PROGID_STR PROGID_STR_W
+#else
+#  define PROGID_STR PROGID_STR_A
+#endif
 
 #define GUID_STRLEN 39
 
@@ -174,7 +182,7 @@ STDAPI DllUnregisterServer(VOID)
         pcr->Release();
     }
 
-    SHDeleteKey(HKEY_CLASSES_ROOT, TEXT(PROGID_STR));
+    SHDeleteKey(HKEY_CLASSES_ROOT, PROGID_STR);
 
     UnregisterProgID(CLSID_VLCPlugin, 2);
     UnregisterProgID(CLSID_VLCPlugin2, 1);
@@ -187,8 +195,8 @@ static HRESULT RegisterClassID(HKEY hParent, REFCLSID rclsid, unsigned int versi
     TCHAR progId[sizeof(PROGID_STR)+16];
     _stprintf(progId, TEXT("%s.%u"), PROGID_STR, version);
 
-    TCHAR description[sizeof(DESCRIPTION)+16];
-    _stprintf(description, TEXT("%s v%u"), DESCRIPTION, version);
+    TCHAR description[sizeof(DESCRIPTION)/sizeof(TCHAR)+16];
+    _stprintf(description, TEXT("%s v%u"), TEXT(DESCRIPTION), version);
 
     HKEY hClassKey;
     {
@@ -211,7 +219,7 @@ static HRESULT RegisterClassID(HKEY hParent, REFCLSID rclsid, unsigned int versi
         }
         if( isDefault )
         {
-            hProgKey = keyCreate(HKEY_CLASSES_ROOT, TEXT(PROGID_STR));
+            hProgKey = keyCreate(HKEY_CLASSES_ROOT, PROGID_STR);
             if( NULL != hProgKey )
             {
                 // default key value
@@ -278,7 +286,7 @@ static HRESULT RegisterClassID(HKEY hParent, REFCLSID rclsid, unsigned int versi
         // VersionIndependentProgID key value
         keyClose(keySetDef(keyCreate(hClassKey,
                                      TEXT("VersionIndependentProgID")),
-                           TEXT(PROGID_STR), sizeof(TEXT(PROGID_STR))));
+                           PROGID_STR, sizeof(PROGID_STR)));
 
         // Version key value
         keyClose(keySetDef(keyCreate(hClassKey,TEXT("Version")),TEXT("1.0")));
